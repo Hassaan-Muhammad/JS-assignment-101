8999e46109e9d750cd80f7d30c1ad72b10eaeda7
muhammadarif2 <mu295752@gmail.com>
2022-05-20 22:09:49 +0500

    added chp4

---
+++ chp 4/app.js
@@ -0,0 +1,15 @@
+//declare 3 variable
+//let name , age , group;
+
+//5 legeal and 5 illegal variable
+//legal
+//let a , myHome, myCar, age, name;
+
+//illegel
+//let class, 7, 7even;
+
+//rulesForNamingJS
+// document.write("<h1>Rules for naming JS variables</h1><br>");
+// document.write("A variable name can contain only letters, numbers, dollar signs, and underscores.");
+// document.write("<br> For example: This is My 1st_veriable$,<br>Variable must begin with a letter,_ or $.For example: $letter,_letter or Letter");
+// document.write("Variable names are case sensitive <br> Variables names should not be JS keywords");
