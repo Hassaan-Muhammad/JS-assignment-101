c7e68f772bb0cbd403bb87a24e1a2c6e799317b1
hassaan <hassaans299@gmail.com>
2022-05-20 21:23:54 +0500

    added chp1

---
+++ chp 1/app.js
@@ -0,0 +1,17 @@
+
+
+// alert("Error! please enter a valid password")
+
+
+// alert("Welcome to JS land.. \n Happy coding !" )
+
+
+// alert("Welcome to JS land..")
+// alert("Happy coding!  \n prevent this page from creating additional dialogs " )
+
+
+
+
+
+
+
