06d99a5caa5c56a9f1279b800abe5c6ddc0d20ac
hassaan <hassaans299@gmail.com>
2022-05-25 21:55:59 +0500

    update

--- chp 1/app.js
+++ chp 1/app.js
@@ -1,13 +1,14 @@
 
+alert("Assalamalikum");
 
-// alert("Error! please enter a valid password")
-
-
-// alert("Welcome to JS land.. \n Happy coding !" )
+alert("Error! please enter a valid password");
 
 
-// alert("Welcome to JS land..")
-// alert("Happy coding!  \n prevent this page from creating additional dialogs " )
+alert("Welcome to JS land.. \n Happy coding !" );
+
+
+alert("Welcome to JS land..");
+alert("Happy coding!  \n prevent this page from creating additional dialogs " );
 
 
 
--- chp 2/app.js
+++ chp 2/app.js
@@ -1,34 +1,35 @@
-// let myName= "Hassaan Muhammad"
+let myName= "Hassaan Muhammad"
 
 
-// let message= "hello world";
-// alert(message); 
-
-
-// let name = "jhone dae"
-// let age = 15
-// let qualification = "Certified mobile and web developer"
-// alert(name);
-// alert(age);
-// alert(qualification);
+let message= "hello world";
+alert(message); 
 
 
 
-// let  pizza = "PIZZA \n PIZZ \n PIZ \n PI \n P "
-// alert(pizza);
+let name = "jhone dae"
+let age = 15
+let qualification = "Certified mobile and web developer"
+alert(name);
+alert(age);
+alert(qualification);
 
 
 
-// let email= "exampla@example.com"
-// alert("my email address is " + email)
+let  pizza = "PIZZA \n PIZZ \n PIZ \n PI \n P "
+alert(pizza);
 
 
-// let book= "A smarter way to learn javascript "
-// alert("I am trying to learn from a Book" + " " + book )
+
+let email= "exampla@example.com"
+alert("my email address is " + email)
 
 
-// document.write("yah! i can write HTML content through javascript")
+let book= "A smarter way to learn javascript "
+alert("I am trying to learn from a Book" + " " + book )
 
 
-//  let design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬ "
-//  alert(design)
+document.write("yah! i can write HTML content through javascript")
+
+
+ let design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬ "
+ alert(design)
